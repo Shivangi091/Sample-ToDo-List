7802dd2885fceb625534aa84b4e29401ec2558f8
Shivangi Rai <shivangirai037@gmail.com>
2017-11-03 12:57:49 -0400

    First Commit

---
+++ src/gae/java/todo/Gae_java_todoServlet.java
@@ -0,0 +1,12 @@
+package gae.java.todo;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Gae_java_todoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
